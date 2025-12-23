365be8587752d6b88839c00436fe600156c07534
NZIMA Ivan <nzimaivan007@gmail.com>
2025-12-23 12:49:36 +0100

    feat: add comments, posts, tags, and users modules with CRUD operations

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,80 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Post } from '../../generated/prisma/client';
+
+@Injectable()
+export class PostsService {
+  constructor(private prisma: PrismaService){}
+
+  async create(createPostDto: CreatePostDto): Promise<Post> {
+    try {
+      return await this.prisma.post.create({
+        data: createPostDto
+      })
+    } catch (error) {
+      console.log("🚀 ~ PostsService ~ create ~ error:", error)
+      throw new InternalServerErrorException('Erreur lors de la création du post')
+    }
+  }
+
+  async findAll(): Promise<Post[]> {
+    try {
+      return await this.prisma.post.findMany({
+        include: {
+          author: true,
+          comments: true,
+          tags: true
+        }
+      });
+    } catch (error) {
+      console.log("🚀 ~ PostsService ~ findAll ~ error:", error)
+      throw new InternalServerErrorException('Erreur lors de la récupération de la liste des posts')
+    }
+  }
+
+  async findOne(id: number): Promise<Post> {
+    try {
+      const post = await this.prisma.post.findUnique({
+        where: {id},
+        include: {
+          author: true,
+          comments: true,
+          tags: true
+        }
+      })
+
+      if(!post){
+        throw new NotFoundException(`Le post avec l'id ${id} n'existe pas`)
+      }
+
+      return post
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la récupération du post avec l'id ${id}`)
+    }
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
+     try {
+      return await this.prisma.post.update({
+        where: {id},
+        data: updatePostDto
+      })
+
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la mise à jour du post avec l'id ${id}`)
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      await this.prisma.post.delete({
+        where: {id}
+      })
+      return {message: `Post avec l'id ${id} supprimé avec succès`}
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la suppression du post avec l'id ${id}`)
+    };
+  }
+}
---
+++ src/tags/tags.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { UpdateTagDto } from './dto/update-tag.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Tag } from '../../generated/prisma/client';
+
+@Injectable()
+export class TagsService {
+  constructor(private prisma: PrismaService){}
+
+  async create(createTagDto: CreateTagDto): Promise<Tag> {
+    try {
+      return await this.prisma.tag.create({
+        data: createTagDto
+      })
+    } catch (error) {
+      console.log("🚀 ~ TagsService ~ create ~ error:", error)
+      throw new InternalServerErrorException('Erreur lors de la création du tag')
+    }
+  }
+
+  async findAll(): Promise<Tag[]> {
+    try {
+      return await this.prisma.tag.findMany({
+        include: {
+          posts: true
+        }
+      });
+    } catch (error) {
+      console.log("🚀 ~ TagsService ~ findAll ~ error:", error)
+      throw new InternalServerErrorException('Erreur lors de la récupération de la liste des tags')
+    }
+  }
+
+  async findOne(id: number): Promise<Tag> {
+    try {
+      const tag = await this.prisma.tag.findUnique({
+        where: {id},
+        include: {
+          posts: true
+        }
+      })
+
+      if(!tag){
+        throw new NotFoundException(`Le tag avec l'id ${id} n'existe pas`)
+      }
+
+      return tag
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la récupération du tag avec l'id ${id}`)
+    }
+  }
+
+  async update(id: number, updateTagDto: UpdateTagDto): Promise<Tag> {
+     try {
+      return await this.prisma.tag.update({
+        where: {id},
+        data: updateTagDto
+      })
+
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la mise à jour du tag avec l'id ${id}`)
+    }
+  }
+
+  async remove(id: number) {
+    try {
+      await this.prisma.tag.delete({
+        where: {id}
+      })
+      return {message: `Tag avec l'id ${id} supprimé avec succès`}
+    } catch (error) {
+      throw new InternalServerErrorException(`Erreur lors de la suppression du tag avec l'id ${id}`)
+    };
+  }
+}
